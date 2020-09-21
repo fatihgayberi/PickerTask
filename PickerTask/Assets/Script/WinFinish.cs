abb0d731eb40a356c2b3b0b5e650d41b33765fc1
Fatih Gayberi <fatihgayberii@gmail.com>
2020-09-21 21:37:56 +0300

    FinishWin
    Oyunun sonuna tebrik ortami eklendi.

---
+++ PickerTask/Assets/Script/WinFinish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinFinish : MonoBehaviour
+{
+    [SerializeField] GameObject finishTxt;
+    [SerializeField] GameObject confettiParticle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finishTxt.gameObject.SetActive(true);
+        ConfettiMaker();
+    }
+
+    void ConfettiMaker()
+    {
+        Instantiate(confettiParticle, new Vector3(2f, -0.5f, 117f), Quaternion.identity);
+        Instantiate(confettiParticle, new Vector3(-2f, -0.5f, 117f), Quaternion.identity);
+    }
+}
